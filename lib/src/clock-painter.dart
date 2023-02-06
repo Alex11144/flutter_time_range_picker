fc4e27a3a629c8f9342a5657ea7d0ff9b320e615
Alex11144 <70289425+Alex11144@users.noreply.github.com>
2023-02-06 14:16:01 +0400

    Update clock-painter.dart

--- lib/src/clock-painter.dart
+++ lib/src/clock-painter.dart
@@ -156,8 +156,8 @@ class ClockPainter extends CustomPainter {
     List.generate(ticks!, (i) => i + 1).forEach((i) {
       if (i == 0 || i % 6 == 0) {
         double angle = (360 / ticks!) * i * pi / 180 + offsetRad;
-        canvas.drawLine(calcCoords(radius, radius, angle, radius + ticksOffset - strokeWidth*4),
-            calcCoords(radius, radius, angle, radius + ticksOffset - strokeWidth/4 + ticksLength*2), paint);
+        canvas.drawLine(calcCoords(radius, radius, angle, radius + ticksOffset - strokeWidth),
+            calcCoords(radius, radius, angle, radius + ticksOffset - strokeWidth + ticksLength*2), paint);
       } else {
         double angle = (360 / ticks!) * i * pi / 180 + offsetRad;
         canvas.drawLine(calcCoords(radius, radius, angle, r),
